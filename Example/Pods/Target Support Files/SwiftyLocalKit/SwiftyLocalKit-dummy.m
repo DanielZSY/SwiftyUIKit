7797a818c3480bdcaeb4451dad0aefad4add914d
DanielZSY <danielzsy@126.com>
2021-05-18 12:01:34 +0800

    0.0.2

--- Example/Pods/Target Support Files/SwiftyLocalKit/SwiftyLocalKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyLocalKit : NSObject
-@end
-@implementation PodsDummy_SwiftyLocalKit
-@end
--- Example/Pods/Target Support Files/SwiftyUIKit/SwiftyUIKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyUIKit : NSObject
-@end
-@implementation PodsDummy_SwiftyUIKit
-@end
